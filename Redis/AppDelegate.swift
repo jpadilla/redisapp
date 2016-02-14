6fe40d7e999388e486b17a0a7ef726b8ac784baf
José Padilla <jpadilla@webapplicate.com>
2016-02-13 23:21:21 -0400

    Initial Commit

---
+++ Redis/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Redis
+//
+//  Created by José Padilla on 2/13/16.
+//  Copyright © 2016 José Padilla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
